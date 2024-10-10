781a635a50e8f833caf150a82319c996179a38ed
Whizma <isak@maatta.se>
2024-10-10 09:51:04 +0200

    speeeed

--- lab6/mmop.c
+++ lab6/mmop.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <arm_neon.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 
-#define N (2048)
-#define BLOCK_SIZE (32)
+#define N (4 * 2048)
+#define BLOCK_SIZE (16)
 
-float sum;
+float sum = 0.0;
 float a[N][N] __attribute__((aligned(32))); // Ensure data is aligned for SIMD
 float b[N][N] __attribute__((aligned(32)));
 float c[N][N] __attribute__((aligned(32)));
@@ -24,18 +27,29 @@ void matmul()
 		}
 	}
 
-#pragma omp parallel for private(i, j, k, ii, jj, kk) schedule(static)
+#pragma omp parallel for private(i, j, k, ii, jj, kk) schedule(guided)
     for (ii = 0; ii < N; ii += BLOCK_SIZE) {
         for (jj = 0; jj < N; jj += BLOCK_SIZE) {
             for (kk = 0; kk < N; kk += BLOCK_SIZE) {
-                // Multiply the blocks
                 for (i = ii; i < ii + BLOCK_SIZE && i < N; i++) {
                     for (j = jj; j < jj + BLOCK_SIZE && j < N; j++) {
-                        float sum = 0.0;
-                        for (k = kk; k < kk + BLOCK_SIZE && k < N; k++) {
-                            sum += b[i][k] * c[k][j];
+                        float32x4_t sum_vec = vdupq_n_f32(0.0);
+						float32x4_t b_vec[2];
+        				float32x4_t c_vec[2];
+
+                        for (k = kk; k < kk + BLOCK_SIZE && k < N; k += 8) {
+
+                            float32x4_t b_vec1 = vld1q_f32(&b[i][k]);
+							float32x4_t b_vec2 = vld1q_f32(&b[i][k+4]);
+							float32x4_t c_vec1 = vld1q_f32(&c[k][j]);
+							float32x4_t c_vec2 = vld1q_f32(&c[k+4][j]);
+							
+							sum_vec = vmlaq_f32(sum_vec, b_vec1, c_vec1);
+							sum_vec = vmlaq_f32(sum_vec, b_vec2, c_vec2);
                         }
-                        a[i][j] += sum;
+
+                        // Horizontally add the elements of sum_vec and store in a[i][j]
+                        a[i][j] += vaddvq_f32(sum_vec);  // Sum all elements of sum_vec
                     }
                 }
             }
@@ -43,10 +57,12 @@ void matmul()
     }
 }
 
+
+
 void init()
 {
 	int i, j;
-
+	#pragma omp parallel for collapse(2)
 	for (i = 0; i < N; i += 1)
 	{
 		for (j = 0; j < N; j += 1)
@@ -60,7 +76,6 @@ void init()
 void check()
 {
 	int i, j;
-
 	for (i = 0; i < N; i += 1)
 		for (j = 0; j < N; j += 1)
 			sum += a[i][j];
